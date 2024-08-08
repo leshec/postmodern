aaec67b86c241b5558f02ff4569ad3396cf1924f
leshec <none>
2024-08-08 16:16:52 +0100

    changed port

--- src/main.rs
+++ src/main.rs
@@ -66,7 +66,7 @@ async fn main() {
     let app = app.fallback(handler_404);
 
     // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:42069")
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:10000")
         .await
         .unwrap();
     axum::serve(listener, app).await.unwrap();
